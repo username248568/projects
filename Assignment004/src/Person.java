840c7ba49e3b3670f119ddc7b4c1944838cb81e6
Serge de Guzman <sergedg27@gmail.com>
2021-03-08 14:13:16 +0800

    First Commit

---
+++ Assignment004/src/Person.java
@@ -0,0 +1,10 @@
+public class Person{
+	private String firstame;
+	private String lastname;
+	public String getfirstname() {
+		return this.firstname
+	}
+	public String getlastname() {
+		return this.firstname()
+	}	
+}
